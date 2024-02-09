8776be4b9f11099967f59c835f11bb03719ea490
Raz <gptraz@gmail.com>
2024-02-08 22:14:16 -0600

    final

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -20,7 +20,7 @@ const progressVariance = cva("h-full w-full flex-1 bg-primary transition-all", {
 
 export interface ProgressProps 
   extends React.HTMLAttributes<HTMLDivElement>,
-    VariantProps<typeof progressVariants> {}
+    VariantProps<typeof progressVariance> {}
 
 type CombinedProgressProps = ProgressProps & React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
 
